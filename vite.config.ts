9381989732854e502e9f65033d0d83641348f298
Shakil Ahmmed <shakilahmmedwakil.dev@gmail.com>
2026-02-27 14:27:08 +0600

    fix (deploy): Made it compatible with vercel

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,14 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
 
 const config = defineConfig({
   plugins: [
     devtools(),
+    nitro({
+      preset: 'vercel',
+    }),
     contentCollections(),
     tsconfigPaths({ projects: ['./tsconfig.json'] }),
     tailwindcss(),
