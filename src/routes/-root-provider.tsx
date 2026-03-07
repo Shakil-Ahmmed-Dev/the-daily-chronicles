df550d43e04c416adbd7a904d1d545fe415e79d0
Shakil Ahmmed <shakilahmmedwakil.dev@gmail.com>
2026-03-07 19:07:56 +0600

    feat: enhance RootProvider by adding TooltipProvider and Toaster components

--- src/routes/-root-provider.tsx
+++ src/routes/-root-provider.tsx
@@ -1,9 +1,16 @@
+import { Toaster } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
 import { getContext } from "@/lib/tanstack-query";
 import { QueryClientProvider } from "@tanstack/react-query";
 
 export function RootProvider({ children }: { children: React.ReactNode }) {
   const { queryClient } = getContext();
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        {children}
+        <Toaster richColors closeButton />
+      </TooltipProvider>
+    </QueryClientProvider>
   );
 }
